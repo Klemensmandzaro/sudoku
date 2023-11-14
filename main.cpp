49d53ee57752d359403a2eeef7864b943fb6bfd2
Klemensmandzaro <kamil.klemiato@wp.pl>
2023-11-14 12:04:33 +0100

    initial commit

--- main.cpp
+++ main.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
 #include <Windows.h>
+#include <conio.h>
 using namespace std;
+#define KEY_UP 72
+#define KEY_DOWN 80
+#define KEY_LEFT 75
+#define KEY_RIGHT 77
+void set_cursor(int,int);
+void printArray(int sudoku[][9]);
+int i, j;
 int main() {
     int sudoku[9][9]={
             {1,2,3,4,5,6,7,8,9,},
@@ -15,6 +23,37 @@ int main() {
     };
     int a;
     system("chcp 437");
+    system("cls");
+    printArray(sudoku);
+    int x=1 , y=1;
+    int c;
+    while (true)
+    {
+        set_cursor(x,y);
+        switch(c=getch())
+        {
+            case KEY_UP:
+                if(y>1) y-=2; break;
+            case KEY_DOWN:
+                if(y<=15) y+=2; break;
+            case KEY_LEFT:
+                if(x>1) x-=2; break;
+            case KEY_RIGHT:
+                if(x<=15) x+=2; break;
+            default:
+                if (isdigit(c))
+                {
+                    cout << char(c);
+                    sudoku[(y-1)/2][(x-1)/2]=c;
+                }
+        }
+    }
+    cin>>a;
+    return 0;
+}
+
+void printArray(int sudoku[][9])
+{
     for (int i=0; i<=18; i++)
     {
         for (int j=0; j<=18; j++)
@@ -117,6 +156,14 @@ int main() {
         }
         cout<<endl;
     }
-    cin>>a;
-    return 0;
+}
+
+void set_cursor(int x = 0 , int y = 0)
+{
+    HANDLE handle;
+    COORD coordinates;
+    handle = GetStdHandle(STD_OUTPUT_HANDLE);
+    coordinates.X = x;
+    coordinates.Y = y;
+    SetConsoleCursorPosition ( handle , coordinates );
 }
